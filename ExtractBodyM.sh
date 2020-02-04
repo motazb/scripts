5e1fab1f691509e5b6a1c413ad489c7d20578822
Moataz Basyouni <mb.motaz@yahoo.com>
2020-02-03 22:51:25 -0800

    hw 5 part 1 complete

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,1 +1,1 @@
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ';' -f 2-6 | tr -s ';' ' ' | sort -r -n -k 6 > BodyM.csv
+tail -n +2 $1 | cut -d $3 -f 2-6 | tr -s $3 ' ' | sort -r -n -k 6 > $2 
